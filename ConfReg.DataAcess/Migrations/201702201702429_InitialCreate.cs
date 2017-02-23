943c8fc847819d802013cda1399307a0d0247891
lemanas <lemanas95@gmail.com>
2017-02-23 20:21:23 +0200

    enabled migrations, deleted edit and details views and controllers, some attributes

---
+++ ConfReg.DataAcess/Migrations/201702201702429_InitialCreate.cs
@@ -0,0 +1,37 @@
+namespace ConfReg.DataAcess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Participants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Degree = c.Int(nullable: false),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        Institution = c.String(),
+                        Email = c.String(),
+                        PhoneNumber = c.String(),
+                        AnnName = c.String(),
+                        AnnAuthorsAndAff = c.String(),
+                        AnnSummary = c.String(),
+                        RoomType = c.Int(nullable: false),
+                        AccPerson = c.Int(nullable: false),
+                        InvoiceReq = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Participants");
+        }
+    }
+}
--- ConfReg.Domain/Participants.cs
+++ ConfReg.Domain/Participants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,39 @@ namespace ConfReg.Domain
     public class Participants
     {
         public int Id { get; set; }
+
         public Degree Degree { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
         public string Institution { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
+        
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
         public string AnnName { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
         public string AnnAuthorsAndAff { get; set; }
+
+        [Required(ErrorMessage = "Šis laukas yra privalomas")]
+        [StringLength(400, ErrorMessage ="Santrauka turi būti ne ilgesnė kaip 400 simbolių")]
         public string AnnSummary { get; set; }
+
         public RoomType RoomType { get; set; }
+
         public AccPerson AccPerson { get; set; }
-        public InvoiceReq InvoiceReq { get; set; }
+
+        public string InvoiceReq { get; set; }
     }
 }
