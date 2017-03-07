1a546567370deb4d1a62904df64fde2547d59c78
lemanas <lemanas95@gmail.com>
2017-03-07 20:18:43 +0200

    all files pushed

---
+++ ConfReg.DataAcess/Migrations/201703032156332_MaxWordsAttrFinal.cs
@@ -0,0 +1,16 @@
+namespace ConfReg.DataAcess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MaxWordsAttrFinal : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ ConfReg.Domain/CustomAttributes/ContainsCharAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace ConfReg.Domain.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    public sealed class ContainsCharAttribute : ValidationAttribute
+    {
+        private readonly char _requiredCharacter;
+
+        public ContainsCharAttribute(char requiredCharacter)
+        {
+            _requiredCharacter = requiredCharacter;
+        }
+
+        public override bool IsValid(object value)
+        {
+            bool result = Convert.ToString(value).Contains(_requiredCharacter);
+            return result;
+        }
+    }
+}
